e02f0de31a1818b9daecc26db30a6c753c0e7ad4
emancu <emiliano.mancuso@gmail.com>
2015-04-19 16:00:10 -0300

    Write some Graph functions

--- Funcional/src/Grafo.hs
+++ Funcional/src/Grafo.hs
@@ -10,27 +10,31 @@ instance (Show a) => Show (Grafo a) where
 
 -- Ejercicio 1
 vacio :: Grafo a
-vacio = undefined
+vacio = G [] (\_ -> [])
 
 -- Ejercicio 2
 nodos :: Grafo a -> [a]
-nodos = undefined
+nodos (G ns _) = ns
 
 -- Ejercicio 3
 vecinos :: Grafo a -> a -> [a]
-vecinos = undefined
+vecinos (G _ f) nodo = f nodo
 
 -- Ejercicio 4
-agNodo :: a -> Grafo a -> Grafo a
-agNodo = undefined
+agNodo :: Eq a => a -> Grafo a -> Grafo a
+agNodo n (G ns f) = G (add_if_not_present n ns) f
 
 -- Ejercicio 5
 sacarNodo :: a -> Grafo a -> Grafo a
-sacarNodo = undefined
+sacarNodo a (G ns f) = undefined
 
 -- Ejercicio 6
-agEje :: (a,a) -> Grafo a -> Grafo a
-agEje = undefined
+agEje :: Eq a => (a,a) -> Grafo a -> Grafo a
+agEje (s, d) (G ns f) | valid_nodes = G ns new_f
+                      | otherwise = G ns f
+    where
+      valid_nodes = elem s ns && elem d ns
+      new_f = (\x -> if x == s then add_if_not_present d (f x) else (f x))
 
 -- Ejercicio 7
 lineal :: [a] -> Grafo a
@@ -47,4 +51,9 @@ clausura = undefined
 
 
 
+---- PRIVATE FUNCTIONS ----
 
+-- Agrega un elemento a una lista si este no pertenece a la misma
+add_if_not_present :: Eq a => a -> [a] -> [a]
+add_if_not_present n ns | n `elem` ns = ns
+                        | otherwise   = n:ns
