b4d01ac5f9b4e56205e992f35a86a8f38ba186fc
emancu <emiliano.mancuso@gmail.com>
2015-04-20 16:37:49 -0300

    Add some tests for Visibilidad and Extraer

--- Funcional/src/Tests.hs
+++ Funcional/src/Tests.hs
@@ -9,18 +9,20 @@ t = runTestTT allTests
 
 allTests = test [
   "parser" ~: testsParser,
-  "grafo" ~: testsGrafo
+  "grafo" ~: testsGrafo,
+  "lomoba" ~: testsLomoba
   ]
 
 testsParser = test [
-  (Var "p")             ~=? (parse "p"),
-  (And (Var "p") (Var "q"))     ~=? (parse "p && q"),
-  (Or (Var "p") (Var "q"))     ~=? (parse "p || q"),
+  (Var "p")                       ~=? (parse "p"),
+  (And (Var "p") (Var "q"))       ~=? (parse "p && q"),
+  (Or (Var "p") (Var "q"))        ~=? (parse "p || q"),
   (Or (Not (Var "p")) (Var "q"))  ~=? (parse "!p || q"),
   (And (D (Var "p")) (Var "q"))   ~=? (parse "<>p && q"),
   (And (B (Var "p")) (Var "q"))   ~=? (parse "[]p && q"),
   (D (And (Var "p") (Var "q")))   ~=? (parse "<>(p && q)"),
-  (B (And (Var "p") (Var "q")))   ~=? (parse "[](p && q)")]
+  (B (And (Var "p") (Var "q")))   ~=? (parse "[](p && q)")
+  ]
 
 testsGrafo = test [
   [1] ~~? (nodos (agNodo 1 vacio)),
@@ -30,6 +32,24 @@ testsGrafo = test [
   [] ~~? (vecinos (agEje (2,1) (agNodo 1 vacio)) 1),
   [] ~~? (vecinos (agEje (1,1) (agNodo 1 vacio)) 2),
   [1] ~~? (vecinos (agEje (1,1) (agNodo 1 vacio)) 1)
+  ]
+
+
+
+testsLomoba = test [
+  -- Test Visibilidad --
+  0 ~=? visibilidad (parse "p"),
+  1 ~=? visibilidad (parse "<>p"),
+  2 ~=? visibilidad (parse "<>!<>p"),
+  2 ~=? visibilidad (parse "<><>p || <><>q"),
+  3 ~=? visibilidad (parse "<>(<>p || <><>q)"),
+  3 ~=? visibilidad (parse "[](<>p || <>[]q)"),
+
+  -- Test Extraer --
+  ["p"]      ~~? extraer (parse "p"),
+  ["p"]      ~~? extraer (parse "p || !p"),
+  ["p", "q"] ~~? extraer (parse "p && q")
+
   ]
 
 ---------------
