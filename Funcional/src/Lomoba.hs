74c05df26ee08073af021b80a423483ced13d0d3
slambrisca <santiagolambrisca@gmail.com>
2015-04-20 10:48:40 -0300

    agrego el fold de Exp y una funcion mas

--- Funcional/src/Lomoba.hs
+++ Funcional/src/Lomoba.hs
@@ -6,16 +6,30 @@ import Tipos
 -- ---------------------------------Sección 6--------- Lomoba ---------------------------
 
 -- Ejercicio 10
---foldExp :: ...
-foldExp = undefined
+foldExp :: (Prop -> b) ->  (b -> b) -> (b -> b -> b) -> (b -> b -> b) -> (b -> b) -> (b -> b) -> Exp -> b
+foldExp fV fNot fOr fAnd fD fB exp = let foldExpRec = (foldExp fV fNot fOr fAnd fD fB) in
+										 case exp of
+											(Var p) -> (fV p)
+											(Not e) -> (fNot (foldExpRec e))
+											(Or e1 e2) -> (fOr (foldExpRec e1) (foldExpRec e2))
+											(And e1 e2) -> (fAnd (foldExpRec e1) (foldExpRec e2))
+											(D e) -> (fD (foldExpRec e))
+											(B e) -> (fB (foldExpRec e))
+
+
+sacarRepetidos :: Eq a => [a] -> [a]
+sacarRepetidos ls = filter ((flip elem) ls) ls
 
 -- Ejercicio 11
 visibilidad :: Exp -> Integer
-visibilidad = undefined
+visibilidad = foldExp (\p -> 0) id (+) (+) (+1) (+1)
+
+exp1 = (Var "p")
 
 -- Ejercicio 12
 extraer :: Exp -> [Prop]
-extraer = undefined
+extraer e = (foldExp (\p -> [p]) id (++) (++) id id e)
+-- Falta asegurarse que no devuelva repetidos
 
 -- Ejercicio 13
 eval :: Modelo -> Mundo -> Exp -> Bool
